124c481be0e6a912135655add8d6e1132bb8944e
Leong Jean Cheong <jeancheong_99@hotmail.com>
2020-08-29 16:07:07 +0800

    Update NonPreemptiveSJF.java
    Rewrite algorithm

--- NonPreemptiveSJF.java
+++ NonPreemptiveSJF.java
@@ -1,57 +1,84 @@
 import java.util.*;
-import java.util.Map.*;
-import java.util.HashMap.*;
-import java.util.LinkedList.*;
+import java.text.DecimalFormat;
 
-public class NonPreemptiveSJF{
-    int position = 0;
-    int count = 0;
-    int accumulator = 0;
+class NonPreemptiveSJF{
+    private LinkedHashMap<String, Integer> sortedArrival;
+    private List<Integer> arrValue;
+    private Set<String> arrKey;
+    private List<String> arrKeyList;
+    private List<String> execProcess = new ArrayList<>();
+    private List<Integer> timestamp = new ArrayList<>();
+    private List<Integer> burstValue = new ArrayList<>();
+    private LinkedHashMap<String, Integer> sortedBurst = new LinkedHashMap();
+    private Set<String> SBKey;
+    private List<String> SBKeyList;
+
+    private int position = 0;
+    private int prevPosition;
+    private int count = 0;
+    private int accumulator = 0;
+    private int currArrTime;
+
     public NonPreemptiveSJF() {}
 
-    public void simulate(LinkedHashMap<String, Integer> arrivalTime, LinkedHashMap<String, Integer> burstTime, int numProcess){
-        LinkedHashMap<String, Integer> sortedArrival = sortByValue(numProcess, arrivalTime);
-        List<Integer> arrValue = new ArrayList<>(sortedArrival.values());
-        Set<String> arrKey = sortedArrival.keySet(); 
-        List<String> arrKeyList = new ArrayList<>(arrKey);
-        List<String> execProcess = new ArrayList<>();
-        List<Integer> timestamp = new ArrayList<>();
-        List<Integer> burstValue = new ArrayList<>();
+    public void schedule(LinkedHashMap<String, Integer> arrivalTime, LinkedHashMap<String, Integer> burstTime, int numProcess){
+        sortedArrival = sortByValue(numProcess, arrivalTime);
+        arrValue = new ArrayList<>(sortedArrival.values());
+        arrKey = sortedArrival.keySet(); 
+        arrKeyList = new ArrayList<>(arrKey);
         
-        for(int i=0; i<numProcess-1; i++){
-            if(arrValue.get(i) == arrValue.get(i+1)){
-                count++;
-                position = i+1;
+        for(int a=0; a<= position; a++) {
+            currArrTime = arrValue.get(position);
+            prevPosition = position;
+            for(int i=position; i<numProcess-1; i++) {
+                if(currArrTime == arrValue.get(i+1)) {
+                    //System.out.println("yes"+i);
+                    count++;
+                    position = i+1;
+                }
+                else{
+                    position++;
+                    //System.out.println("no");
+                    break;
+                }     
             }
-        }
-        if(position == 0){
-            execProcess.add(arrKeyList.get(0));
-        }
-        for(int i=0; i<position; i++){
-            if(burstTime.get(arrKeyList.get(i)) < burstTime.get(arrKeyList.get(i+1))){
-                execProcess.add(arrKeyList.get(i));
-                execProcess.add(arrKeyList.get(i+1));
+            if(count == 0) {
+                //System.out.println("count==0");
+                //System.out.println(arrKeyList.get(prevPosition));
+                execProcess.add(arrKeyList.get(prevPosition));               
             }
-            else if(burstTime.get(arrKeyList.get(i)) > burstTime.get(arrKeyList.get(i+1))){
-                    execProcess.add(arrKeyList.get(i+1));
-                    execProcess.add(arrKeyList.get(i));
+            else{
+                //System.out.println(count);
+                for(int k=0; k<=count; k++, prevPosition++) {
+                    //System.out.println(prevPosition);
+                    sortedBurst.put(arrKeyList.get(prevPosition), burstTime.get(arrKeyList.get(prevPosition)));
+                    //System.out.println(sortedBurst.toString());
+                }
+                sortedBurst = sortByValue(count, sortedBurst);
+                SBKey = sortedBurst.keySet();
+                SBKeyList = new ArrayList<>(SBKey);
+                //System.out.println("SBKeyList " +SBKeyList.toString());
+
+                for(int m=0; m<SBKeyList.size(); m++) {
+                    execProcess.add(SBKeyList.get(m));
+                    //System.out.println(execProcess.toString());
+                }                
             }
-        }
-        for(int i=0; i<numProcess; i++){
-            execProcess.add(arrKeyList.get(i));
+            count = 0; 
+            sortedBurst.clear(); 
         }
 
-        for(int i=0; i<numProcess; i++){
+        for(int i=0; i<numProcess; i++) {
             int temp = burstTime.get(execProcess.get(i));
             burstValue.add(i,temp);
         }
         timestamp.add(0,0);
-        for(int i=1; i<numProcess; i++){
+        for(int i=0; i<numProcess; i++){
             accumulator += burstValue.get(i);
-            timestamp.add(i, accumulator);
+            timestamp.add(i+1, accumulator);
         }
-        ganttChart(execProcess, timestamp, numProcess);
-    }   
+        ganttChart(numProcess);
+    }  
     public LinkedHashMap<String, Integer> sortByValue(int numProcess, LinkedHashMap<String, Integer> temp){
         List<Map.Entry<String, Integer>> entries = new ArrayList<>(temp.entrySet());
         Collections.sort(entries, new Comparator<Map.Entry<String, Integer>>() {
@@ -68,14 +95,14 @@ public class NonPreemptiveSJF{
 
         return temp;
     }
-    public void ganttChart(List<String> execProcess, List<Integer> timestamp, int numProcess){
+    public void ganttChart(int numProcess){
         for(int i=0; i<numProcess; i++){
             System.out.print("|---" + execProcess.get(i) + "---");
         }
         System.out.println("|");
-        for(int i=0; i<numProcess; i++){
-            System.out.print(timestamp.get(i) + "        ");
+        for(int i=0; i<=numProcess; i++){
+            System.out.print(new DecimalFormat("00").format(timestamp.get(i)) + "       ");
         }
         System.out.print("\n");
-    }
+    }       
 }
