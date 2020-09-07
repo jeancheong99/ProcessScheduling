fab89a6c8c41a318e9a987ed6bd51bb9a78e7ffe
Leong Jean Cheong <jeancheong_99@hotmail.com>
2020-09-07 16:15:07 +0800

    Update NonPreemptivePriority.java

--- NonPreemptivePriority.java
+++ NonPreemptivePriority.java
@@ -2,14 +2,24 @@ import java.util.*;
 import java.text.DecimalFormat;
 
 class NonPreemptivePriority {
-    private LinkedHashMap<String, Integer> sortedArrival;
+    DecimalFormat df = new DecimalFormat("00");
+
+    private LinkedHashMap<String, Integer> sortedArrival = new LinkedHashMap<>();;
     private List<Integer> arrValueList;
     private Set<String> arrKey;
     private List<String> arrKeyList;
 
+    private LinkedHashMap<String, Integer> finishTime = new LinkedHashMap<>();
+    private Set<String> ftKey;
+    private List<String> ftKeyList;
+
+    private LinkedHashMap<String, Integer> turnaroundTime = new LinkedHashMap<>();
+    private LinkedHashMap<String, Integer> waitingTime = new LinkedHashMap<>();
+
     private List<String> readyQueue = new ArrayList<>();
     private List<String> executionList = new ArrayList<>();
     private List<Integer> timestamp = new ArrayList<>();
+
 
     private int numProcess;
     private int counter = 0;
@@ -79,7 +89,6 @@ class NonPreemptivePriority {
                     executionList.add(readyQueue.get(0));
                     currTime += burstTime.get(readyQueue.get(0));
                     timestamp.add(counter, currTime);
-                    //currTime += burstTime.get(readyQueue.get(0));
                     arrKeyList.remove(readyQueue.get(0));
                     arrValueList.remove(0);
                     readyQueue.clear();
@@ -89,7 +98,6 @@ class NonPreemptivePriority {
                     executionList.add(readyQueue.get(1));
                     currTime += burstTime.get(readyQueue.get(1));
                     timestamp.add(counter, currTime);
-                    //currTime += burstTime.get(readyQueue.get(1));
                     arrKeyList.remove(readyQueue.get(1));
                     arrValueList.remove(1);
                     readyQueue.clear();
@@ -100,13 +108,20 @@ class NonPreemptivePriority {
                 executionList.add(arrKeyList.get(arrKeyList.size()-1));
                 currTime += burstTime.get(arrKeyList.get(arrKeyList.size()-1));
                 timestamp.add(counter, currTime);
-                //currTime += burstTime.get(arrKeyList.get(arrKeyList.size()-1));
                 arrKeyList.remove(arrKeyList.size()-1);
                 arrValueList.remove(arrValueList.size()-1);
                 counter++;
             }
         }
+        for(int i=1; i < timestamp.size(); i++){
+            finishTime.put(executionList.get(i-1), timestamp.get(i));
+        }
+        ftKey = finishTime.keySet();
+        ftKeyList = new ArrayList<>(ftKey);
         ganttChart();
+        turnaroundTimeCalc(arrivalTime);
+        waitingTimeCalc(burstTime);
+        printCalcTable(arrivalTime, burstTime);
     }  
     public LinkedHashMap<String, Integer> sortByValue(LinkedHashMap<String, Integer> temp){
         List<Map.Entry<String, Integer>> entries = new ArrayList<>(temp.entrySet());
@@ -130,9 +145,48 @@ class NonPreemptivePriority {
         }
         System.out.println("|");
         for(int i=0; i<=numProcess; i++){
-            System.out.print(new DecimalFormat("00").format(timestamp.get(i)) + "       ");
+            System.out.print(df.format(timestamp.get(i)) + "       ");
         }
         System.out.print("\n");
     }       
-
+    public void turnaroundTimeCalc(LinkedHashMap<String,Integer> arrivalTime) {
+        // Finish time - Arrival time
+        int finish;
+        int arr;
+        for(int i=0; i < numProcess; i++) {
+            finish = finishTime.get(ftKeyList.get(i));
+            arr = arrivalTime.get(executionList.get(i));
+            turnaroundTime.put(ftKeyList.get(i), (finish-arr)); 
+        }
+    }
+    public void waitingTimeCalc(LinkedHashMap<String,Integer> burstTime) {
+        // turnaround time - burst time
+        int turnaround;
+        int burst;
+        for(int i=0; i < numProcess; i++) {
+            turnaround = turnaroundTime.get(("P"+i));
+            burst = burstTime.get(("P"+i));
+            waitingTime.put(("P"+i), (turnaround-burst));
+        }
+    }
+    public void printCalcTable(LinkedHashMap<String,Integer> arrivalTime, LinkedHashMap<String,Integer> burstTime) {
+        String key;
+        int totalTurnaround = 0;
+        int totalWaiting = 0;
+        System.out.print("\n");
+        System.out.println("|-----------|--------------|------------|-------------|-----------------|--------------|");
+        System.out.println("|  Process  | Arrival Time | Burst Time | Finish Time | Turnaround Time | Waiting Time |");
+        System.out.println("|-----------|--------------|------------|-------------|-----------------|--------------|");
+        for(int i=0; i < numProcess; i++) {
+            key = "P"+i;
+            System.out.println("|  " + key + "       | " + arrivalTime.get(key) + "            | " + burstTime.get(key) + "          | " +
+                               df.format(finishTime.get(key)) + "          | " + df.format(turnaroundTime.get(key)) + "              | " +
+                               df.format(waitingTime.get(key)) + "           |" );
+            totalTurnaround += turnaroundTime.get(key);
+            totalWaiting += waitingTime.get(key);
+        }
+        System.out.println("|--------------------------------------------------------------------------------------|");
+        System.out.println("Average Turnaround time: " + (totalTurnaround/numProcess));
+        System.out.println("Average Waiting time   : " + (totalWaiting/numProcess));
+    }
 }
