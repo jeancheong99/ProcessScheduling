a4f29b39f3f776448b75698302729f684610f358
Leong Jean Cheong <leongjc26@gmail.com>
2020-09-28 15:12:44 +0800

    Update NonPreemptiveSJF.java

--- NonPreemptiveSJF.java
+++ NonPreemptiveSJF.java
@@ -236,8 +236,10 @@ class NonPreemptiveSJF{
             totalWaiting += waitingTime.get(process);
         }
         System.out.println("|--------------------------------------------------------------------------------------|");
-        System.out.println("Average Turnaround time: " + (totalTurnaround/numProcess));
-        System.out.println("Average Waiting time   : " + (totalWaiting/numProcess));
+        System.out.println("Total Turnaround Time  : " + totalTurnaround);
+        System.out.println("Average Turnaround Time: " + (totalTurnaround/numProcess));
+        System.out.println("\nTotal Waiting Time   : " + totalWaiting);
+        System.out.println("Average Waiting Time   : " + (totalWaiting/numProcess));
     }
 
 
